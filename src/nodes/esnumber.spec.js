27aa6dac395d482d279ef4b52135d314bf0e4390
kwangure <kaf.wangure@gmail.com>
2023-01-18 21:57:34 +0300

    test: check for string coercability

--- src/nodes/esnumber.spec.js
+++ src/nodes/esnumber.spec.js
@@ -4,11 +4,16 @@ import { ESNumber } from 'src/nodes/esnumber.js';
 describe('ESNumber', () => {
 	it('coerces non-numbers to numbers', () => {
 		const number = new ESNumber('3');
-		expect(number.valueOf()).toBe(3);
+		expect(number.get()).toBe(3);
 
 		// @ts-expect-error
 		// `&` does type coercion
 		expect(number & 1).toBe(1);
+	});
+
+	it('is coercable to string', () => {
+		const number = new ESNumber('3');
+		expect(number.get() + 'abc').toBe('3abc');
 	});
 
 	it('is serializable to number', () => {
