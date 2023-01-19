2e0cf34cf536300d28f2a470e60a18f84fdc6181
kwangure <kaf.wangure@gmail.com>
2023-01-19 14:21:13 +0300

    feat: implement node children & parents

--- src/nodes/esnode.spec.js
+++ src/nodes/esnode.spec.js
@@ -1,6 +1,20 @@
 import { describe, expect, it } from 'vitest';
 import { ESNode } from './esnode';
 
+/**
+ * @typedef {import('type-fest').JsonValue} JsonValue
+ */
+
+/**
+ * @template T
+ * @extends {ESNode<T>}
+ */
+class ESNewNode extends ESNode {
+	toJSON() {
+		return /** @type {JsonValue} */(super.get());
+	}
+}
+
 describe('ESNode', () => {
 	it('returns untransformed values', () => {
 		// @ts-expect-error
@@ -45,7 +59,9 @@ describe('ESNode', () => {
 					super.dispatchEvent(event, value);
 				}
 			}
-			toJSON() { return /** @type {import('type-fest').JsonValue} */(super.get()) }
+			toJSON() {
+				return /** @type {JsonValue} */(super.get())
+			}
 		}
 
 		const node = new ESNewNode('3');
@@ -54,6 +70,48 @@ describe('ESNode', () => {
 	});
 
 	it.todo('calls subsbribers', () => {
+
+	});
+
+	describe('handles child and parent nodes', () => {
+		const child1 = new ESNewNode('1');
+		const child2 = new ESNewNode('2');
+		const child3 = new ESNewNode('3');
+		const parentNode = new ESNewNode('4');
+
+		let childValues = '';
+		child1.subscribe((value) => childValues += value);
+		child2.subscribe((value) => childValues += value);
+		child3.subscribe((value) => childValues += value);
+
+		let parentValues = '';
+		parentNode.subscribe((value) => parentValues += value);
+
+		it('sets parent & child nodes', () => {
+			parentNode.append(child1, child2, child3);
+
+			expect(child1.parentNode).toBe(parentNode);
+			expect(child2.parentNode).toBe(parentNode);
+			expect(child3.parentNode).toBe(parentNode);
+			expect(childValues).toBe('123123');
+
+			expect(parentNode.children.length).toBe(3);
+			expect(parentNode.children.at(0)).toBe(child1);
+			expect(parentNode.children.at(1)).toBe(child2);
+			expect(parentNode.children.at(2)).toBe(child3);
+			expect(parentValues).toBe('44');
+		});
+
+		it('removes parent & child nodes', () => {
+			parentNode.remove(child1, child2, child3);
+			expect(child1.parentNode).toBe(null);
+			expect(child2.parentNode).toBe(null);
+			expect(child3.parentNode).toBe(null);
+			expect(childValues).toBe('123123123');
+
+			expect(parentNode.children.length).toBe(0);
+			expect(parentValues).toBe('444');
+		});
 
 	});
 });
--- src/nodes/esnode.ts
+++ src/nodes/esnode.ts
@@ -1,7 +1,11 @@
 import type { JsonValue } from "type-fest";
 import { isESNodeType } from "./esnodeutils";
 
+const _SET_PARENT = Symbol('set-parent');
+
 export abstract class ESNode<T> {
+	#children: Set<ESNode<any>> = new Set();
+	#parentNode: ESNode<any> | null = null;
 	#subscribers: Set<(arg: T) => any> = new Set();
 	#value;
 	constructor(value: T) {
@@ -12,6 +16,9 @@ export abstract class ESNode<T> {
 			return suscriber(this.#value);
 		}
 	}
+	get children() {
+		return [...this.#children];
+	}
 	dispatchEvent(event: string, value: T): any {
 		if (event === 'set') {
 			this.#value = value;
@@ -20,6 +27,9 @@ export abstract class ESNode<T> {
 	}
 	get() {
 		return this.#value;
+	}
+	get parentNode() {
+		return this.#parentNode;
 	}
 	set(value: T) {
 		if (!isESNodeType(this, ESNode)) return;
@@ -31,4 +41,31 @@ export abstract class ESNode<T> {
 		return () => this.#subscribers.delete(fn);
 	}
 	abstract toJSON() : JsonValue;
+	append(...nodes: ESNode<any>[]): void {
+		if (!nodes.length) return;
+
+		for (const node of nodes) {
+			node[_SET_PARENT](this);
+			this.#children.add(node);
+		}
+
+		this.#callSubscribers();
+	}
+	remove(...nodes: ESNode<any>[]): void {
+		if (!nodes.length) return;
+
+		let deleted = 0;
+		for (const node of nodes) {
+			if (this.#children.delete(node)) {
+				node[_SET_PARENT](null);
+				deleted += 1
+			}
+		}
+
+		if (deleted) this.#callSubscribers();
+	}
+	[_SET_PARENT](node: ESNode<any> | null): void {
+		this.#parentNode = node;
+		this.#callSubscribers();
+	}
 }
