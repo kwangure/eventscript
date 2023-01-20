9cc44350c770a7d683201315fe384a431f4e109e
kwangure <kaf.wangure@gmail.com>
2023-01-20 21:36:03 +0300

    feat: initial boolean implementation

---
+++ src/nodes/esboolean.js
@@ -0,0 +1,41 @@
+import { bubbleChange } from "./esnodeutils";
+import { ESNode } from "./esnode";
+import { NODE_VALUE } from "./esnode_constants";
+
+/**
+ * @extends {ESNode<boolean>}
+ */
+export class ESBoolean extends ESNode {
+	/**
+	 * @param {any} value
+	 */
+	constructor(value) {
+		const boolean = Boolean(value);
+		super(boolean);
+		this[NODE_VALUE] = boolean;
+	}
+	/**
+	 * @param {any} value
+	 */
+	set(value) {
+		const boolean = Boolean(value);
+		if (this[NODE_VALUE] === boolean) return;
+		super.set(boolean);
+		bubbleChange(this);
+	}
+	false() {
+		this.set(false);
+	}
+	toggle() {
+		this.set(!this[NODE_VALUE]);
+	}
+	true() {
+		this.set(true);
+	}
+	toJSON() {
+		return this[NODE_VALUE];
+	}
+	[Symbol.toPrimitive]() {
+		return this[NODE_VALUE];
+	}
+}
---
+++ src/nodes/esboolean.spec.js
@@ -0,0 +1,42 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { ESBoolean } from './esboolean';
+import { NODE_VALUE } from './esnode_constants';
+
+describe('ESBoolean', () => {
+	/**
+	 * @type {ESBoolean}
+	 */
+	let boolean;
+
+	beforeEach(() => {
+		boolean = new ESBoolean(3);
+	})
+
+	it('coerces non-booleans to booleabs', () => {
+		expect(boolean[NODE_VALUE]).toBe(true);
+	});
+
+	it('is coercable to string', () => {
+		expect(boolean[NODE_VALUE] + 'abc').toBe('trueabc');
+	});
+
+	it('is serializable to string', () => {
+		expect(JSON.stringify(boolean)).toEqual('true');
+	});
+
+	it('creates new ESBoolean from exisiting ESBooleans', () => {
+		const booleanAlso = new ESBoolean(boolean);
+		expect(booleanAlso[NODE_VALUE]).toBe(true);
+		expect(boolean).toEqual(booleanAlso);
+		expect(boolean).not.toBe(booleanAlso);
+	});
+
+	it('does not call subscribers on set unless value changed', () => {
+		let booleanvalue = '';
+		boolean.subscribe((value) => booleanvalue += value);
+		expect(booleanvalue).toBe('true');
+
+		boolean.set(true);
+		expect(booleanvalue).toBe('true');
+	});
+});
